e221b985f2573ce3feaff4ef990e668e84bb5023
luishs0 <luishs.doc@gmail.com>
2022-11-28 17:14:35 +0100

    Milestone 2

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
-$length = intval($_GET["length"] ?? 0);
-
-$caracters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabdcefghijklmnñopqrstuvwxyz0123456789+*^-_<>¿?¡![]#$%&/()={}";
-$caracters_length = strlen($caracters);
-$password = "";
-
-function createPassword($length, $caracters, $caracters_length, $password)
-{
-
-    for ($i = 0; $i < $length; $i++) {
-        $cur_caracter = $caracters[rand(0, ($caracters_length - 1))];
-        $password = $password . $cur_caracter;
-    }
-    return $password;
-}
-
+include __DIR__ . "/functions.php";
 ?>
 
 
